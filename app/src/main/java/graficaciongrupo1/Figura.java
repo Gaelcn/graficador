b9a9370c5b09ecb4814783b2c71a02671b686c7b
Gaelcn <23680038@cuautla.tecnm.mx>
2025-02-28 15:10:47 -0600

    Add files via upload

---
+++ app/src/main/java/graficaciongrupo1/Figura.java
@@ -0,0 +1,70 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package graficaciongrupo1;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import javax.swing.DefaultListModel;
+
+/**
+ *
+ * @author contr
+ */
+public class Figura {
+    
+    public static int escala = 20; 
+    
+    private String nombre;
+    private DefaultListModel<Punto> puntos;
+    
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public DefaultListModel<Punto> getPuntos() {
+        if(puntos == null){
+            puntos = new DefaultListModel<>();
+        }
+        return puntos;
+    }
+
+    public void setPuntos(DefaultListModel<Punto> puntos) {
+        this.puntos = puntos;
+    }
+
+    public Figura(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    public void Dibujar(ShapeRenderer shpRenderer){
+        for(int i = 0; i < getPuntos().size(); i++){
+            Punto p =getPuntos().get(i);
+            p.Dibujar(shpRenderer);
+        }
+        
+        if(getPuntos().size() > 1){
+            shpRenderer.setColor(Color.RED);
+            for(int i = 0; i < getPuntos().size()-1; i++){
+                Punto p1 =getPuntos().get(i);
+                Punto p2 =getPuntos().get(i+1);
+                shpRenderer.rectLine(p1.getpX()*Figura.escala, p1.getpY()*Figura.escala, p2.getpX()*Figura.escala, p2.getpY()*Figura.escala,5);
+            }
+        } 
+    }
+
+    @Override
+    public String toString() {
+        return getNombre();
+    }
+    
+    
+    
+}
---
+++ app/src/main/java/graficaciongrupo1/PanelCanvas.java
@@ -0,0 +1,85 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package graficaciongrupo1;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import javax.swing.DefaultListModel;
+
+/**
+ *
+ * @author libookami
+ */
+public class PanelCanvas implements ApplicationListener {
+    SpriteBatch sptBatch;
+    BitmapFont bmpFont;
+    ShapeRenderer shpRenderer;
+
+    DefaultListModel<Figura> listaFiguras;
+
+    public PanelCanvas() {
+        listaFiguras = new DefaultListModel<>();
+        System.out.println("mianbibsubi");
+    }
+    
+    @Override
+    public void create() {
+        System.out.println("create()");
+        sptBatch = new SpriteBatch();
+        bmpFont = new BitmapFont();
+        shpRenderer = new ShapeRenderer();
+    }
+
+    @Override
+    public void resize(int i, int i1) {
+    }
+
+    @Override
+    public void render() {
+        Gdx.gl.glClearColor(0f, 0f, 0f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        
+        shpRenderer.begin(ShapeRenderer.ShapeType.Filled);
+        
+        for(int j = 0; j < listaFiguras.getSize(); j++){
+            listaFiguras.get(j).Dibujar(shpRenderer);
+        }
+        shpRenderer.end();
+       
+        shpRenderer.begin(ShapeRenderer.ShapeType.Line);
+        shpRenderer.setColor(Color.WHITE);
+        for(int i = 0; i*Figura.escala < Gdx.graphics.getHeight(); i++){
+            shpRenderer.line(i*Figura.escala,0,i*Figura.escala,Gdx.graphics.getHeight());
+            shpRenderer.line(0,i*Figura.escala,Gdx.graphics.getWidth(),i*Figura.escala);
+        }
+        shpRenderer.end();
+        
+    }
+
+    @Override
+    public void pause() {
+        System.out.println("pause()");
+        System.out.println("prueba()");
+    }
+
+    @Override
+    public void resume() {
+        System.out.println("resume()");
+    }
+
+    @Override
+    public void dispose() {
+        System.out.println("dispose()");
+        sptBatch.dispose();
+        bmpFont.dispose();
+        shpRenderer.dispose();
+    }
+    
+}
